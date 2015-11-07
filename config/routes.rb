ad0ffc6f5d782712540ec1a40a0d6994936f2075
Victor Zeng <tkezm977@gmail.com>
2015-11-07 16:57:22 -0500

    hot fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'experiences#index'
 
-  resources :experiences, only: [:index]
+  resources :experiences
 
   get  '/neighborhoods' => 'home#neighborhoods'
 
