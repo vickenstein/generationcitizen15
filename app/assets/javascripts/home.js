4d51d437d250b5f152843c4b70ee02bfefe826df
Victor Zeng <tkezm977@gmail.com>
2015-11-07 13:53:46 -0500

    added some stuff

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,6 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
